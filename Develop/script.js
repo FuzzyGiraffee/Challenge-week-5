317316b75b38a85d13a8bbfbecb63c559f9dff59
FuzzyGiraffee <121840200+FuzzyGiraffee@users.noreply.github.com>
2023-02-27 12:04:45 +1400

    Site Fully Functional
    Site functionality completed. README must be completed before submission.

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,47 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 var saveButton = $(".btn.saveBtn.col-2.col-md-1")
-console.log(saveButton)
-
-
-
-$(function () {
-
-//display current day on header
 var currentDay = dayjs().format('dddd, MMMM D');
 
+$(function () {
+//display current day on header
+var saveButton = $(".btn.saveBtn.col-2.col-md-1")
 $('#currentDay').text(currentDay)
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  //Event listener
-    $.each(saveButton,btn =>{
-
-      console.log(btn)
-      (btn).addEventListener("click", function(){
-        var test = this.parentElement.id
-         console.log (test)
+  //Selects every button
+    saveButton.each((i,btn) =>{
+      //adds event listen to each button
+      btn.addEventListener("click", function(){
+        var hourId = this.parentElement.id
+        var userInput = this.parentElement.children[1].value
+        console.log(userInput)
+        localStorage.setItem(hourId, userInput)
+        console.log(hourId)
        })
-
     })
+  
+  $('.row.time-block').each((i,row) => {
+
+    //grab time in dayjs hour format
+    var rowTime = Number(row.id[row.id.length-1])
+    rowTime += 9
     
+    //grab current time
+    var currentTime = dayjs().hour()
+
+    //change class name dynamiclly to set color
+    if (rowTime === currentTime){
+      $(row).addClass('present')
+    } else if(rowTime > currentTime){
+      $(row).addClass('future')
+    } else{
+      $(row).addClass('past')
+    }
+
+    var storedText = localStorage.getItem(row.id)
     
+    $(row).children()[1].value = storedText
+
+  })
    
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+
 });
 
-
-
-
-/* grab id name, use that as variable to store into local storage
-    
-
-
-*/
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Challenge-week-5
+# Daily Schedule
+
+![Website](/Develop/Website-Demo.png)
+
+## Features
+
+
+
+## Installation
+
+
+No installation required, hosted at 
+
+
+## Usage
+
+N/A
+
+## Contributing
+
+Not currently accepting help as this website is a project from UTSA FUll Stack Bootcamp.
+
+## License
+
+
+N/A
